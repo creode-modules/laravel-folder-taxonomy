38ccdac7f4ca713ca9072ae5362bfec223eea5ff
Jamie Sykes <jaymeh@users.noreply.github.com>
2023-12-19 15:39:57 +0000

    Adds a repository for Folders.

---
+++ src/Repositories/FolderRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Creode\LaravelFolderTaxonomy;
+
+use Creode\LaravelRepository\BaseRepository;
+
+class FolderRepository extends BaseRepository
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getModel(): string
+    {
+        return config('folder-taxonomy.folder_model');
+    }
+}
